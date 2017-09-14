2c77d6631f59d13d39acbe76fe2bcb12be2e773a
AbdoulBW <abdoulbwane@gmail.com>
2017-09-13 18:28:47 -0700

    Added login
    Added loging functionality now

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(require("express-session")({
     secret:"On ne baissera pas les bras",
     resave:false,
@@ -25,6 +25,8 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
+// ROUTES
+
 app.get('/', function (req,res) {
     res.render("home")
 });
@@ -32,6 +34,31 @@ app.get('/', function (req,res) {
 app.get('/secret', function (req,res) {
     res.render("secret")
 });
+
+
+//Auth routes
+//show signup form
+app.get('/register', function (req,res) {
+    res.render('register')
+});
+
+//submit form
+app.post('/register', function (req,res) {
+    var uname = req.body.username;
+    var pword = req.body.password;
+    User.register(new User({username:req.body.username}),req.body.password, function (err,user) {
+        if(err){
+            console.log(err)
+            res.render('register');
+        }else{
+            passport.authenticate("local")(req,res, function () {
+                res.redirect("/secret");
+            })
+        }
+    }
+)
+    
+})
 
 
 app.listen(3000, function () {
